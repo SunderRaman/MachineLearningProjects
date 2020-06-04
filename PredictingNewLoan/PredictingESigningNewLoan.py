fb4dd99372a0c426bef4e8c28ea98cb00e8f8f06
Sunder Raman Venkataraman <sunderramanv@yahoo.com>
2020-06-04 19:18:46 +0530

    Source Code file
    Source Code file

--- PredictingNewLoan/PredictingESigningNewLoan.py
+++ PredictingNewLoan/PredictingESigningNewLoan.py
@@ -1,1 +1,481 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Predicting eSigning of New Loans
+
+# In[1]:
+
+
+import pandas as pd
+from dateutil import parser
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import seaborn as sn
+import os
+import time
+import matplotlib
+import random
+
+
+# In[2]:
+
+
+# Set the working directory & Read the data
+os.chdir("C:\\Sunder\\DataScience\\MachineLearning\\Projects\\PredictingNewLoan")
+dataset = pd.read_csv("Loan-Financial-Data.csv")
+
+
+# In[3]:
+
+
+# Basic analysis of the data
+dataset.shape
+dataset.isna().any()
+dataset.head()
+
+
+# In[7]:
+
+
+# Plotting Histograms
+dataset2 = dataset.drop(columns = ['entry_id', 'pay_schedule', 'e_signed'])
+
+fig = plt.figure(figsize=(15, 12))
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(dataset2.shape[1]):
+    plt.subplot(6, 3, i + 1)
+    f = plt.gca()
+    f.set_title(dataset2.columns.values[i])
+
+    vals = np.size(dataset2.iloc[:, i].unique())
+    if vals >= 100:
+        vals = 100
+    
+    plt.hist(dataset2.iloc[:, i], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+
+# In[9]:
+
+
+# Correlation of the varaiables with the response variable (esigned)
+dataset2.corrwith(dataset.e_signed).plot.bar(
+        figsize = (20, 10), title = "Correlation with E Signed", fontsize = 15,
+        rot = 45, grid = True)
+# All the correlation values are ranging between -0.1 to 0.1. 
+# One of the columns "months_employed" is least correlated and hence can be deleted for further analysis
+
+
+# In[10]:
+
+
+## Plot the correlation matrix
+
+sn.set(style="white")
+
+# Compute the correlation matrix
+corr = dataset2.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(18, 15))
+
+# Generate a custom diverging colormap
+cmap = sn.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask 
+sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
+# # Feature Engineering and Data Preparation
+
+# In[4]:
+
+
+# Feature Engineering
+dataset = dataset.drop(columns = ['months_employed'])
+
+# Combining account_m and account_y to account_months
+dataset['personal_account_months'] = (dataset.personal_account_m + (dataset.personal_account_y * 12))
+dataset = dataset.drop(columns = ['personal_account_m', 'personal_account_y'])
+
+
+# In[5]:
+
+
+# One hot encoding and viewing the columns
+dataset = pd.get_dummies(dataset)
+dataset.columns
+# Drop semi_monthly pay schedule, entry_id (taken into another var) and target varaible(e_signed)
+response = dataset["e_signed"]
+users = dataset['entry_id']
+dataset = dataset.drop(columns = ["e_signed", "entry_id","pay_schedule_semi-monthly"])
+
+
+# In[6]:
+
+
+# Splitting into Train and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset,
+                                                    response,
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+
+
+# In[7]:
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+# Column names and indexes are lost during scaling.Reverting them back
+X_train2.columns = X_train.columns.values
+X_test2.columns = X_test.columns.values
+X_train2.index = X_train.index.values
+X_test2.index = X_test.index.values
+X_train = X_train2
+X_test = X_test2
+
+
+# # Model Building
+
+# In[8]:
+
+
+## Logistic Regression (Model-1)
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0, penalty = 'l2', solver = 'lbfgs')
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+
+# In[9]:
+
+
+# Tabulate the results
+results = pd.DataFrame([['Linear Regression ', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+results
+
+
+# In[10]:
+
+
+## SVM (Linear) (Model-2)
+from sklearn.svm import SVC
+classifier = SVC(random_state = 0, kernel = 'linear')
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['SVM (Linear)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+results = results.append(model_results, ignore_index = True)
+
+
+# In[11]:
+
+
+results
+
+
+# In[12]:
+
+
+## SVM (RBF) (Model-3)
+from sklearn.svm import SVC
+classifier = SVC(random_state = 0, kernel = 'rbf')
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['SVM (rbf)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+
+results = results.append(model_results, ignore_index = True)
+
+
+# In[13]:
+
+
+results
+
+
+# In[14]:
+
+
+# Random Forest Classifier (Model-4)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(random_state = 0, n_estimators = 100,
+                                    criterion = 'entropy')
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest (n=100)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+results = results.append(model_results, ignore_index = True)
+
+
+# In[15]:
+
+
+results
+
+
+# In[42]:
+
+
+## K-fold Cross Validation for the 
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X= X_train, y = y_train,
+                             cv = 10)
+print("Random Forest Classifier Accuracy: %0.2f (+/- %0.2f)"  % (accuracies.mean(), accuracies.std() * 2))
+
+
+# In[41]:
+
+
+pip install joblib
+
+
+# In[16]:
+
+
+# Applying Grid Search on Random Forest Classifier (Model-4)
+# Varying various parameters and applying the criterion (Entropy and Gini)
+# Round 1: Entropy
+parameters = {"max_depth": [3, None],
+              "max_features": [1, 5, 10],
+              'min_samples_split': [2, 5, 10],
+              'min_samples_leaf': [1, 5, 10],
+              "bootstrap": [True, False],
+              "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier, 
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1,
+                           verbose = 1)
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+# In[17]:
+
+
+# Predicting Test Set based on 1st round parameters
+y_pred = grid_search.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest (n=100, Grid Search#1 + Entropy)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+results = results.append(model_results, ignore_index = True)
+
+
+# In[18]:
+
+
+results
+
+
+# In[19]:
+
+
+# Round 2: Entropy with varied parameters
+parameters = {"max_depth": [None],
+              "max_features": [3, 5, 7],
+              'min_samples_split': [8, 10, 12],
+              'min_samples_leaf': [1, 2, 3],
+              "bootstrap": [True],
+              "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier, # Make sure classifier points to the RF model
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1,
+                           verbose = 1)
+
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+# In[20]:
+
+
+# Predicting Test Set based on 2nd round parameters
+y_pred = grid_search.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest (n=100, Grid Search#2 + Entropy)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+results = results.append(model_results, ignore_index = True)
+results
+
+
+# In[21]:
+
+
+# Round 1: Gini
+parameters = {"max_depth": [3, None],
+              "max_features": [1, 5, 10],
+              'min_samples_split': [2, 5, 10],
+              'min_samples_leaf': [1, 5, 10],
+              "bootstrap": [True, False],
+              "criterion": ["gini"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1,
+                           verbose = 1)
+
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+# In[23]:
+
+
+#  Round 2 (Gini)
+parameters = {"max_depth": [None],
+              "max_features": [8, 10, 12],
+              'min_samples_split': [2, 3, 4],
+              'min_samples_leaf': [8, 10, 12],
+              "bootstrap": [True],
+              "criterion": ["gini"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier, # Make sure classifier points to the RF model
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1,
+                           verbose=1)
+
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+# In[24]:
+
+
+# Predicting Test Set (with Gini)
+y_pred = grid_search.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest (n=100, Round#2 + Gini)', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+results = results.append(model_results, ignore_index = True)
+
+
+# In[25]:
+
+
+results
+
+
+# In[36]:
+
+
+results
+
+
+# In[37]:
+
+
+# Plotting Confusion Matrix
+# Evaluating Results
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
+precision_score(y_test, y_pred) # tp / (tp + fp)
+recall_score(y_test, y_pred) # tp / (tp + fn)
+f1_score(y_test, y_pred)
+#Displaying the confusion matrix
+df_cm = pd.DataFrame(cm, index = (0, 1), columns = (0, 1))
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)
+sn.heatmap(df_cm, annot=True, fmt='g')
+
+
+# In[ ]:
+
+
+
 
